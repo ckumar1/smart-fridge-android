aa0fb4845375eb1055406d78bc917389d4a897cb
Carl Pratt <carl.pratt.13@gmail.com>
2014-03-24 18:42:32 -0500

    Added the MainActivity class, finished work on the DatabaseHandler, and added Food and Recipe classes

--- src/com/example/smart_fridge_android/MainActivity.java
+++ src/com/example/smart_fridge_android/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.smart_fridge_android;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends ListActivity {
+
+public class MainActivity extends Activity {
+
+    DatabaseHandler db;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        db = new DatabaseHandler(this);
+        db.getDatabaseName();
+    }
+
+    public void onButtonClick (View v){
+
     }
 }
