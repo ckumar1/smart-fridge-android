09066eea64d59fa5aff04234041bb1c48e6aa47e
Cory Groom <corypgr@hotmail.com>
2014-03-25 18:47:26 -0500

    First try at Add food manual

--- src/com/example/smart_fridge_android/AddFoodActivity.java
+++ src/com/example/smart_fridge_android/AddFoodActivity.java
@@ -1,13 +1,57 @@
 package com.example.smart_fridge_android;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
-public class AddFoodActivity extends Activity {
-	
+import android.app.Activity;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.DialogFragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+public class AddFoodActivity extends Activity implements OnDateSetListener{
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_food);
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_food);
+		EditText mDateTextView = (EditText)findViewById(R.id.dateSelector);
+		mDateTextView.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				FragmentTransaction ft = getFragmentManager().beginTransaction();
+				DialogFragment newFragment = new DatePickerDialogFragment(AddFoodActivity.this);
+				newFragment.show(ft, "date_dialog");
+			}
+		});
+	}
+
+	@Override
+	public void onDateSet(DatePicker view, int year, int monthOfYear,
+			int dayOfMonth) {
+		Calendar cal = new GregorianCalendar(year, monthOfYear, dayOfMonth);
+		EditText mDateTextView = (EditText)findViewById(R.id.dateSelector);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		mDateTextView.setText(dateFormat.format(cal.getTime()));
+	}
+
+	public void onButtonClick(View v){
+
+		switch (v.getId()){
+
+		case R.id.addBtn: //Put this code in a modular location
+			Intent i = new Intent(getApplicationContext(), AddFoodActivity.class);
+			startActivity(i);
+			break;
+		case R.id.manualBtn:
+			setContentView(R.layout.add_food_manual);
+			break;
+		}
+	}
 }
