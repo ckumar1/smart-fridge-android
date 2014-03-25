64075f1cf971d0bce24edca2a8008933a6377a55
Katie Hildebrand <khildebrand@wisc.edu>
2014-03-25 17:07:20 -0500

    Added ingredients field, DBHandler.java needs to be updated accordingly

--- src/com/example/smart_fridge_android/Recipe.java
+++ src/com/example/smart_fridge_android/Recipe.java
@@ -1,20 +1,24 @@
 package com.example.smart_fridge_android;
 
+import java.util.List;
+
 public class Recipe {
 
     private int id;
     private String name;
     private String directions;
     private String notes;
+    private String ingredients;
 
     public Recipe(){
 
     }
 
-    public Recipe(String name, String directions, String notes) {
+    public Recipe(String name, String directions, String notes, String ingredients) {
         this.name = name;
         this.directions = directions;
         this.notes = notes;
+        this.ingredients = ingredients;
     }
 
     public int getId() {
@@ -44,6 +48,14 @@ public class Recipe {
     public void setNotes(String notes) {
         this.notes = notes;
     }
+    
+    public String getIngredients() {
+    	return ingredients;
+    }
+    
+    public void setIngredients(String ingredients) {
+    	this.ingredients = ingredients;
+    }
 
     @Override
     public String toString() {
@@ -52,6 +64,7 @@ public class Recipe {
                 ", name='" + name + '\'' +
                 ", directions='" + directions + '\'' +
                 ", notes='" + notes + '\'' +
+                ", ingredients='" + ingredients + '\'' +
                 '}';
     }
 }
