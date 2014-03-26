2a789fb317fd528bd539e34feead063fad64883d
Cory Groom <corypgr@hotmail.com>
2014-03-26 18:47:02 -0500

    fix IndividualRecipeActivity

--- src/com/example/smart_fridge_android/IndividualRecipeActivity.java
+++ src/com/example/smart_fridge_android/IndividualRecipeActivity.java
@@ -9,7 +9,7 @@ public class IndividualRecipeActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_individual_recipe);
+		setContentView(R.layout.individual_recipe_item);
 	}
 
 	@Override
