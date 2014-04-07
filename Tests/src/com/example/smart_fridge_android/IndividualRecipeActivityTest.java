5fd6e8835446ddad76d6136ab6582b33c37e3c8d
Cory Groom <corypgr@hotmail.com>
2014-04-06 22:11:27 -0500

    Add empty test files for all activities. None are currently implemented

---
+++ Tests/src/com/example/smart_fridge_android/IndividualRecipeActivityTest.java
@@ -0,0 +1,18 @@
+package com.example.smart_fridge_android;
+
+import android.test.AndroidTestCase;
+
+public class IndividualRecipeActivityTest extends AndroidTestCase{
+	
+	protected void setUp() throws Exception{
+        super.setUp();
+        //TODO Write Set up code
+    }
+
+    protected void tearDown() throws Exception{
+        super.tearDown();
+        //TODO Write tearDown code
+    }
+
+    //TODO Write Tests
+}
