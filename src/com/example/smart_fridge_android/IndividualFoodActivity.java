809f7f305022cfdf3c8d0dd31f38105cc67d7eef
liana <lliubinskas@wisc.edu>
2014-04-09 16:38:12 -0500

    individual food items are not edible. layout and activity updated

--- src/com/example/smart_fridge_android/IndividualFoodActivity.java
+++ src/com/example/smart_fridge_android/IndividualFoodActivity.java
@@ -1,18 +1,25 @@
 package com.example.smart_fridge_android;
 
+import java.text.SimpleDateFormat;
+import android.app.DatePickerDialog.OnDateSetListener;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
-public class IndividualFoodActivity extends Activity {
+public class IndividualFoodActivity extends Activity implements OnDateSetListener{
 
     private static final String STARTING_TAB = "startingTab";
 
     DatabaseHandler db;
 	Food food = new Food();
-	
+		
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,17 +36,38 @@ public class IndividualFoodActivity extends Activity {
         TextView nameText = (TextView) findViewById(R.id.FoodName);
         nameText.setText(name);
         
+    	       
         int quantity = food.getQuantity();
         TextView quantityText = (TextView) findViewById(R.id.FoodQuantity);
         quantityText.setText(""+quantity);
         
-        String expDate = food.getExpirationDate();
         TextView expDateText = (TextView) findViewById(R.id.ExpDate);
-        expDateText.setText(expDate);
+        expDateText.setText(food.getExpirationDate());
         
-        //add in nutritional information Iteration 2
-        
+        expDateText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                DialogFragment newFragment = new DatePickerDialogFragment(IndividualFoodActivity.this);
+                newFragment.show(ft, "date_dialog");
             }
+        });
+        
+        //nutritional information will be displayed and can edit; need to conntect to API
+        String nutritionalInformation = food.getDescription();
+        TextView nutritionalInfoText = (TextView) findViewById(R.id.nutritionalInformationText);
+        nutritionalInfoText.setText(nutritionalInformation);
+            }
+	
+	@Override
+	public void onDateSet(DatePicker view, int year, int monthOfYear,
+			int dayOfMonth) {
+		Calendar cal = new GregorianCalendar(year, monthOfYear, dayOfMonth);
+		TextView mDateTextView = (TextView)findViewById(R.id.ExpDate);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+		mDateTextView.setText(dateFormat.format(cal.getTime()));
+	}
 	
 //get id, intent to start activity
 	public void onButtonClick(View v) {
@@ -58,7 +86,25 @@ public class IndividualFoodActivity extends Activity {
 			i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             i.putExtra(STARTING_TAB, "food");
 			this.startActivity(i);
-        	break;       
+        	break;      	
+        
+        case R.id.update_button:
+        	String name = ((TextView)findViewById(R.id.FoodName)).getText().toString();
+            food.setName(name);
+                
+            food.setExpirationDate(((TextView)findViewById(R.id.ExpDate)).getText().toString());
+          
+            try {
+                int quantity = Integer.parseInt(((TextView)findViewById(R.id.FoodQuantity)).getText().toString());
+                food.setQuantity(quantity);
+            } catch(NumberFormatException e) {	}
+            
+            String nutrition = ((TextView)findViewById(R.id.nutritionalInformationText)).getText().toString();
+            food.setDescription(nutrition);
+            
+            db.updateFood(food);    
+           
+            break;
         }
 	
 	}	
