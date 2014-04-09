315eea491bdb16429f1f3aefc81b44c472e54a2b
Carl Pratt <carl.pratt.13@gmail.com>
2014-04-08 23:56:05 -0500

    Added secondary call to yummly to aquire more specific recipe information such as instructions url and nutritional information

--- src/com/example/smart_fridge_android/AdvancedSearchRecipeActivity.java
+++ src/com/example/smart_fridge_android/AdvancedSearchRecipeActivity.java
@@ -2,7 +2,6 @@ package com.example.smart_fridge_android;
 
 import android.app.ListActivity;
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,13 +15,22 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class AdvancedSearchRecipeActivity extends ListActivity {
 
     private static final String TAG_RECIPE_NAME = "name";
     private static final String TAG_RECIPE_INGREDIENTS = "ingredients";
-    private static final String APP_ID = "_app_id";
-    private static final String APP_KEY = "_app_key";
+    private static final String TAG_APP_ID = "_app_id";
+    private static final String TAG_APP_KEY = "_app_key";
+    private static final String ACTUAL_APP_ID = "5780cd3c";
+    private static final String ACTUAL_APP_KEY = "c2d2c46e25e5051ee5f51ebedcdd16fd";
+
+    // Holds the recipes from the recipe search
+    List<Recipe> recipes = new ArrayList<Recipe>();
+
+    // Holds the recipe ids from the search to use in getting specific details
+    List<String> yummlyRecipeIds = new ArrayList<String>();
 
     // Progress Dialog
     private ProgressDialog pDialog;
@@ -34,12 +42,18 @@ public class AdvancedSearchRecipeActivity extends ListActivity {
     JSONParser jsonParser = new JSONParser();
 
     Button calorieCountButton;
-
     EditText recipeNameEditText;
+
+    String caloriesFromYummly;
 
     // url to yummly api
     // Using Carl's api id and api key for now.
-    private static String url_yummly = "http://api.yummly.com/v1/api/recipes";
+
+    // Url to perform a search of recipes on yummly's site
+    private static String url_yummly_search_recipes = "http://api.yummly.com/v1/api/recipes";
+
+    // Url to get specific details about a recipe
+    private static String url_yummly_get_recipe_details = "http://api.yummly.com/v1/api/recipe/";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -58,8 +72,6 @@ public class AdvancedSearchRecipeActivity extends ListActivity {
                 prepareSearch();
                 new GetRecipes().execute();
 
-                //parseJson();
-
                 setContentView(R.layout.recipe_search_results);
                 break;
 
@@ -75,6 +87,9 @@ public class AdvancedSearchRecipeActivity extends ListActivity {
         }
     }
 
+    /**
+     * Grab any input that the user has entered and prepare for query
+     */
     private void prepareSearch(){
 
         recipeNameEditText = (EditText) findViewById(R.id.editTextRecipeName);
@@ -114,12 +129,26 @@ public class AdvancedSearchRecipeActivity extends ListActivity {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id)
             {
+
+                String yummlyId = yummlyRecipeIds.get(position);
+
+                try {
+                    // Get specific recipe details
+                    caloriesFromYummly = new GetRecipeDetails(yummlyId).execute().get();
+                } catch (InterruptedException e){
+
+                } catch (ExecutionException e){
+
+                }
+
                 setContentView(R.layout.recipe_result);
                 TextView name = (TextView) findViewById(R.id.textViewRecipeResultName);
                 TextView ingredients = (TextView) findViewById(R.id.textViewIngredientsResultList);
+                TextView calories = (TextView) findViewById(R.id.textViewRecipeSearchCarloriesResultList);
 
                 name.setText(recipes.get(position).getName());
                 ingredients.setText(recipes.get(position).getIngredients());
+                calories.setText(caloriesFromYummly);
             }
         });
     }
@@ -128,8 +157,6 @@ public class AdvancedSearchRecipeActivity extends ListActivity {
      * Background Async Task to execute the yummly query
      * */
     class GetRecipes extends AsyncTask<String, String, String> {
-
-        List<Recipe> recipes = new ArrayList<Recipe>();
 
         /**
          * Before starting background thread Show Progress Dialog
@@ -154,19 +181,20 @@ public class AdvancedSearchRecipeActivity extends ListActivity {
             // Building Parameters
             List<NameValuePair> params = new ArrayList<NameValuePair>();
 
-            params.add(new BasicNameValuePair(APP_ID, "5780cd3c"));
-            params.add(new BasicNameValuePair(APP_KEY, "c2d2c46e25e5051ee5f51ebedcdd16fd"));
+            params.add(new BasicNameValuePair(TAG_APP_ID, ACTUAL_APP_ID));
+            params.add(new BasicNameValuePair(TAG_APP_KEY, ACTUAL_APP_KEY));
             params.add(new BasicNameValuePair("q", recipeName));
             params.add(new BasicNameValuePair("maxResult", Integer.toString(20)));
             params.add(new BasicNameValuePair("start", Integer.toString(20)));
 
             // getting JSON Object
-            json = jsonParser.makeHttpRequest(url_yummly,
+            json = jsonParser.makeHttpRequest(url_yummly_search_recipes,
                     "GET", params);
 
             // check log cat for response
             Log.d("Create Response", json.toString());
 
+            // Gather initial information (name, ingredients) on each recipe returned
             try {
                 JSONArray recipesArray = json.getJSONArray("matches");
 
@@ -187,6 +215,10 @@ public class AdvancedSearchRecipeActivity extends ListActivity {
                     r.setName(name);
                     r.setIngredients(ingredients);
                     recipes.add(r);
+
+                    // Put in the id to use in the GetRecipeDetails class
+                    String yummlyId = recipe.get("id").toString();
+                    yummlyRecipeIds.add(yummlyId);
                 }
 
             } catch (JSONException e) {
@@ -206,6 +238,85 @@ public class AdvancedSearchRecipeActivity extends ListActivity {
             // dismiss the dialog once done
             pDialog.dismiss();
         }
+    }
 
+
+    /**
+     * Background Async Task to execute the yummly query for getting specific recipe details
+     * */
+    class GetRecipeDetails extends AsyncTask<String, String, String> {
+
+        String yummlyId;
+        String calories;
+
+        public GetRecipeDetails(String yummlyId){
+            this.yummlyId = yummlyId;
+        }
+
+        List<Recipe> recipes = new ArrayList<Recipe>();
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(AdvancedSearchRecipeActivity.this);
+            pDialog.setMessage("Gathering your recipe information...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        /**
+         * Performing yummly api query
+         * */
+        protected String doInBackground(String... args) {
+
+            url_yummly_get_recipe_details += yummlyId;
+
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+            params.add(new BasicNameValuePair(TAG_APP_ID, ACTUAL_APP_ID));
+            params.add(new BasicNameValuePair(TAG_APP_KEY, ACTUAL_APP_KEY));
+
+            // getting JSON Object
+            json = jsonParser.makeHttpRequest(url_yummly_get_recipe_details,
+                    "GET", params);
+
+            // check log cat for response
+            Log.d("Create Response", json.toString());
+
+            // Go through all the new data on the recipe from yummly
+            try {
+                JSONArray nutritionEstimates = json.getJSONArray("nutritionEstimates");
+
+                for (int i = 0; i < nutritionEstimates.length(); i++) {
+                    JSONObject individualEstimate = nutritionEstimates.getJSONObject(i);
+
+                    // Looking for calories.
+                    if (individualEstimate.get("attribute").equals("ENERC_KCAL")){
+                        calories = individualEstimate.get("value").toString();
+                    }
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return calories;
+        }
+
+        /**
+         * After completing background task dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+
+            // dismiss the dialog once done
+            pDialog.dismiss();
+
+            caloriesFromYummly = calories;
+        }
     }
 }
