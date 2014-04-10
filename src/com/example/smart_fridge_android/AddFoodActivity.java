e4d1d8936b429d478bfdff1521c322fd2c4781c1
Ryan Misura <misura@wisc.edu>
2014-04-10 18:09:39 -0500

    Creating a food from a barcode works now. Can add it to the database and have it display in your main food list

--- src/com/example/smart_fridge_android/AddFoodActivity.java
+++ src/com/example/smart_fridge_android/AddFoodActivity.java
@@ -1,14 +1,25 @@
 package com.example.smart_fridge_android;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.DialogFragment;
 import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -18,6 +29,11 @@ import android.widget.Toast;
 public class AddFoodActivity extends Activity implements OnDateSetListener{
 
     private static final String STARTING_TAB = "startingTab";
+    private int SCAN_REQUEST_CODE = 0;
+    private ProgressDialog pDialog;
+    JSONParser jsonParser = new JSONParser();
+    String upc_name = "";
+	private String upcResult = "";
 
     @Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -81,13 +97,124 @@ public class AddFoodActivity extends Activity implements OnDateSetListener{
                 i.putExtra(STARTING_TAB, "food");
                 this.startActivity(i);
                 break;
+            case R.id.scanBtn:
+    			scanBarcode();
+    			break;
 		}
 
         removeAddButton();
 	}
+	
+    public void addBarcodeItem() {
+    	Food food = new Food();
+        food.setName(upc_name);
+        food.setExpirationDate("");
+		try {
+			int quantity = 0;
+			food.setQuantity(quantity);
+		} catch(NumberFormatException e) {	}
+		food.setCategory("");
+        
+        DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+		db.addFood(food); 
+		
+		Intent i = new Intent(this, MainActivity.class);
+		i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		this.startActivity(i);
+    }
 
     private void removeAddButton(){
         Button addButton = (Button) findViewById(R.id.addBtn);
         addButton.setVisibility(View.INVISIBLE);
     }
+    
+    public void scanBarcode() {
+        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+        intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+        startActivityForResult(intent, SCAN_REQUEST_CODE); 
+    }
+    
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == SCAN_REQUEST_CODE) {
+           if (resultCode == RESULT_OK) {
+               
+              String contents = intent.getStringExtra("SCAN_RESULT");
+              upcResult = contents; //save away the upc you get from scanning barcode
+            //  String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+           
+              //display results
+              Log.i("result",contents);
+              Toast.makeText(getApplicationContext(), contents, Toast.LENGTH_LONG).show();
+              
+              PerformUpcLookup lookup = new PerformUpcLookup();
+              lookup.execute();
+              
+              
+           } else if (resultCode == RESULT_CANCELED) {
+              // Handle cancel
+              Log.i("App","Scan unsuccessful");
+           }
+      }
+   }
+    
+    
+	class PerformUpcLookup extends AsyncTask<String, String, String> {
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(AddFoodActivity.this);
+            pDialog.setMessage("Getting food from barcode...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        /**
+         * Performing login
+         * */
+        protected String doInBackground(String... args) {
+        	String upc = upcResult;
+        	List<NameValuePair> params = new ArrayList<NameValuePair>();
+        	params.add(new BasicNameValuePair("upc", upc));
+            params.add(new BasicNameValuePair("appId", UpcApis.getNutritionixAppId()));
+            params.add(new BasicNameValuePair("appKey", UpcApis.getNutritionixAppKey()));
+            JSONObject json = jsonParser.makeHttpRequest(UpcApis.getNutritionixUrl(),
+                    "GET", params);
+
+            String item_description = "";
+            String item_name = "";
+            String brand_name = "";
+            try {
+				item_description = json.getString("item_description");
+	            item_name = json.getString("item_name");
+	            brand_name = json.getString("brand_name");
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            
+            upc_name = item_name;
+            // check log cat for response
+            Log.d("Create Response", json.toString());
+            return item_name;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once done
+            pDialog.dismiss();
+            addBarcodeItem();
+        	
+        }
+        
+      
+
+    }
+    
+    
 }
